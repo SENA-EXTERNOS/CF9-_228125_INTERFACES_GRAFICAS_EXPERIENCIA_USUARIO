4efe7c4fcb5952ffbd0291c0c6d90e0cce8e7045
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-03-29 22:33:07 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -17,7 +17,7 @@
 
     figure(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/DDj97-xzxx8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 </template>
--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -74,7 +74,7 @@
 
     figure.mb-4(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/1M9THjuijOA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .row.justify-content-center
       .col-lg-10.cajon.color-primario.p-4(data-aos="flip-up")
